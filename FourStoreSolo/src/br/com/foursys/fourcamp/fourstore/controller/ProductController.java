73519642b3868cd2389b9b44aed6558c6d0ffd98
mariana3000 <105241618+mariana3000@users.noreply.github.com>
2022-06-02 08:40:57 -0300

    initial commit

---
+++ FourStoreSolo/src/br/com/foursys/fourcamp/fourstore/controller/ProductController.java
@@ -0,0 +1,69 @@
+package br.com.foursys.fourcamp.fourstore.controller;
+
+import br.com.foursys.fourcamp.fourstore.model.Product;
+import br.com.foursys.fourcamp.fourstore.service.ProductService;
+
+public class ProductController {
+	String result = null;
+	ProductService service = new ProductService();
+	
+	
+	public String addProduct(String sku, Double price, Integer qtt) {
+		Product product = new Product(sku, price, qtt);
+		
+		if(product.getSku().length() < 12 || product.getSku().length() > 12) {
+			return "ERRO, ESSE SKU NÃO ESTÁ NO PADRÃO DE 12 DÍGITOS";
+		}
+		
+		if(service.callCreate(product)) {
+			if(product.getDepartment() != null || product.getSize() != null || product.getType() != null || product.getColor() != null || product.getCategory() != null) {
+				result = "Cadastro realizado com sucesso."
+						+ "\n Produto SKU: "+product.getSku()
+						+ "\n Preço: "+product.getPrice()
+						+ "\n Quantidade no estoque: "+product.getQtt()
+						+ "\n Tipo: "+product.getType().getDescription()
+						+ "\n Tamanho: "+product.getSize().getDescription()
+						+ "\n Cor: "+product.getColor().getDescription()
+						+ "\n Categoria: "+product.getCategory().getDescription()
+						+ "\n Departamento: "+product.getDepartment().getDescription2();
+			} else {
+				result = "ERRO, ESSE SKU NÃO FOI POSSIVEL CADASTRAR";
+			}
+		}else {
+			result = "Não foi possível cadastrar o produto. Produto já cadastrado.";
+		}
+		return result;
+	}
+	
+	public String callAdd(String sku, Integer qtt) {
+		
+		Product product = new Product(sku, qtt);
+		if(service.callAdd(product)) {
+			result = "Estoque ampliado com sucesso!";
+		}else {
+			result = "Operação inválida. Tente novamente...";
+		}
+		return result;
+	}
+	
+	public String callRemove(String sku, Integer qtt) {
+		Product product = new Product(sku, qtt);
+		if(service.callRemove(product)) {
+			result = "Estoque removido com sucesso!";
+		}else {
+			result = "Operação não foi um sucesso.";
+		}
+		return result;
+	}
+	
+	public String callListAll() {
+		result = "";
+		result = service.callListAll();
+		if(result.equals("")) {
+			result = "Não existe nenhum produto cadastrado";
+			return result;
+		}
+		return result;
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+
+# FourStoreMVP_02
+
+Projeto destinado ao FourCamp (bootcamp) da Foursys, com intuito do desenvolvimento de um sistema de gerenciamento de estoque em Java.
+
+
+
+
+## Autores
+
+- [@edulongodevgeo](https://github.com/edulongodevgeo)
+- [@DenisMarostica](https://github.com/DenisMarostica)
+- [@marc-andrade](https://github.com/marc-andrade)
+- [@mariana3000](https://github.com/mariana3000)
+- [@MateusHEsteves](https://github.com/MateusHEsteves)
+- [@PauloDK001](https://github.com/PauloDK001)
+- [@PedroBueno-tech](https://github.com/PedroBueno-tech)
+- [@Tiago-Prudente](https://github.com/Tiago-Prudente)
+- [@Todymiu10](https://github.com/Todymiu10)
+
+
+
+
+
+
+
+
+
+
+
+
+## Documentação
+
+[Documentação](https://github.com/edulongodevgeo/FourStoreMVP_02/blob/main/Manual%20do%20usu%C3%A1rio%20-%20Fourstore.pdf)
+
+
+
